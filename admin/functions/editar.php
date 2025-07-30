a0d3a860b09854a08d0f96f97df5e0b7e26c0e59
urvalencia <urvalencia@grupomavi.com>
2025-07-29 18:11:46 -0600

    Corrección de errores de CSS en formularios de crear y editar

--- admin/functions/editar.php
+++ admin/functions/editar.php
@@ -76,7 +76,7 @@ foreach($ubicaciones as $ub) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="./editar.css">
+    <link rel="stylesheet" href="./../css/editar.css">
 </head>
 <body>
     <!-- Navbar -->
